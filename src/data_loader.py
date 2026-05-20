7f546cd5837e2cc369c6df5f163262c9a2b7e7b9
Paula Noeller <noellerpaula@gmail.com>
2026-05-20 13:45:37 -0400

    Added GBM vs historical S&P-500 comparison

---
+++ src/data_loader.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import numpy as np
+
+index = "^GSPC"
+start = '2020-01-01'
+def load_logreturns():
+    prices = load_prices()
+    returns = np.log(prices / prices.shift(1)).dropna()
+    return returns
+
+def load_prices():
+    data = yf.download(index, start)
+    prices = data["Close"].squeeze()
+    return prices
---
+++ src/gbm.py
@@ -0,0 +1,56 @@
+
+#We want to build a GBM model fitting our data
+#The stochastic differential equation describing Geometric Brownian motion 
+#is as follows:
+# dS_t = mu*S_tdt + sigma*S_tdZt
+# The solution (found by applying Ito's lemma) of this stochastic differential equation is:
+#S_t = S_0 exp{(mu - 1/2*sigma**2)t + sigma*Z_t} where the distribution of Z_t is N(0,t)
+# this gives us: log S_t -log S_0 is N(mu*t - (1/2 * sigma**2*t),sigma**2*t) distributed
+#where mu is the constant drift, sigma is the constant volatility and Z_t is a standard normally
+# distributed random variable
+import numpy as np
+
+
+#----------------------------------------------------------------------------------------------
+#Simulate GBM path
+#----------------------------------------------------------------------------------------------
+def simulate_GBM(prices, logreturns):
+    #Start price of stock
+    S0 = prices.iloc[0]
+    #-------------
+    # Simulation setup
+    #-----------------
+
+
+    #Number of observations
+    M   = len(prices)
+    # Trading-day convention
+    N = 252 # trading days per year
+    #Total time horizon in years:
+    T = M / N
+
+    #Time grid in years
+    t = np.linspace(0, T, M)
+
+    #Time-step size
+    dt = T /(M-1)
+
+    #Annualized GBM parameters
+    mu = np.mean(logreturns)*N #annual drift
+    sigma = np.std(logreturns)*np.sqrt(N) #annual volatility
+    #-------------------
+    #Simulation
+    #---------------------
+
+    #We generate Gaussian shocks, scaled by time step
+    dW = np.sqrt(dt)*np.random.randn(M-1)
+    #We accumulate the increments to obtain an approximation of Brownian motion
+    #Brownian motion path with W(0) = 0
+    W = np.concatenate([[0], np.cumsum(dW)])
+    X = (
+     (mu - 0.5*sigma**2)*t
+     + sigma * W
+    )
+
+    S = S0*np.exp(X)
+    return S
